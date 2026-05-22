93560415edf28cc4d3366207a7cf72fc2772e026
torgeronimo <victorgeronimod@gmail.com>
2026-05-22 15:37:44 +0800

    fixed spinner broken img

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -1,8 +1,10 @@
+import portal from '../../assets/portal.png';
+
 
 const Spinner = () => {
     return (
         <div>
-            <img src="/src/assets/portal.png" alt="Spinner" className="animate-spin [animation-duration:2s] w-50 h-50 mb-4 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+            <img src={portal} alt="Spinner" className="animate-spin [animation-duration:2s] w-50 h-50 mb-4 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2" />
         </div>
     )
 }
