a9791711129a82ca53b79f8354f5ecf60fb2714e
torgeronimo <victorgeronimod@gmail.com>
2026-05-22 14:37:37 +0800

    finalized

--- src/sections/MoreSection.tsx
+++ src/sections/MoreSection.tsx
@@ -2,8 +2,8 @@ const MoreSection = () => {
     return (
         <section className="section-wrapper h-screen w-full flex flex-col items-center justify-center gap-6">
             <h1 className="text-4xl font-bold text-center">More coming soon!</h1>
-            <img src="/src/assets/logo.svg" alt="Rick and Morty Logo" className='w-250 object-fit' />
-            <img src="/src/assets/shadowrick.png" alt="Rick and Morty Logo" className='h-auto w-70 object-fit'/>
+            <img src="../src/assets/logo.svg" alt="Rick and Morty Logo" className='w-250 object-fit' />
+            <img src="../src/assets/shadowrick.png" alt="Rick and Morty Logo" className='h-auto w-70 object-fit'/>
         </section>
     )
 }
