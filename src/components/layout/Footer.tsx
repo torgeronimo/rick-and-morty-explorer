8c3854a4d90d6343008cc514dfb55fb620970ab8
torgeronimo <victorgeronimod@gmail.com>
2026-05-22 15:19:01 +0800

    fixed src extension name

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,5 @@
-import githubIcon from '../assets/github.svg';
-import linkedinIcon from '../assets/linkedin.svg';
+import githubIcon from '../../assets/github.svg';
+import linkedinIcon from '../../assets/linkedin.svg';
 
 const Footer = () => {
     return (
